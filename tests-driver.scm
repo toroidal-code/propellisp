f6d160ae75445d9ec1c1ebee8af947a9606c9266
Katherine Whitlock <toroidalcode@gmail.com>
2013-06-07 22:20:42 -0400

    patched tests-driver.scm

--- tests-driver.scm
+++ tests-driver.scm
@@ -1,5 +1,16 @@
+
+(define all-tests '())
+
 (define (compile-program x)
   (emit-program x))
+
+(define-syntax add-tests-with-string-output
+  (syntax-rules (=>)
+    [(_ test-name [expr => output-string] ...)
+     (set! all-tests
+        (cons
+           '(test-name [expr string  output-string] ...)
+            all-tests))]))
 
 (define (run-compile expr)
   (let ([p (open-output-file "build/stst.s" 'replace)])
@@ -7,7 +18,7 @@
     (close-output-port p)))
 
 (define (build)
-  (unless (zero? (system "make"))
+  (unless (zero? (system "make all"))
     (error 'make "could not build target")))
 ;;  (unless (zero? (system "gcc -o stst startup.c stst.s"))
 ;;    (error 'make "could not build target")))
@@ -31,6 +42,42 @@
               (cond
                [(eof-object? c) (void)]
                [else (display c) (f)]))))))))
+
+(define (test-with-string-output test-id expr expected-output)
+   (run-compile expr)
+   (build)
+   (execute)
+   (unless (string=? expected-output (get-string))
+     (error 'test (format "output mismatch for test ~s, expected ~s, got ~s"
+        test-id expected-output (get-string)))))
+
+(define (test-one test-id test)
+  (let ([expr (car test)]
+        [type (cadr test)]
+        [out  (caddr test)])
+    (printf "test ~s:~s ..." test-id expr)
+    (flush-output-port)
+    (case type
+     [(string) (test-with-string-output test-id expr out)]
+     [else (error 'test (format "invalid test type ~s" type))])
+    (printf " ok\n")))
+
+(define (test-all)
+  (let f ([i 0] [ls (reverse all-tests)])
+    (if (null? ls)
+        (printf "passed all ~s tests\n" i)
+        (let ([x (car ls)] [ls (cdr ls)])
+          (let* ([test-name (car x)]
+                 [tests (cdr x)]
+                 [n (length tests)])
+            (printf "Performing ~a tests ...\n" test-name)
+            (let g ([i i] [tests tests])
+              (cond
+                [(null? tests) (f i ls)]
+                [else
+                 (test-one i (car tests))
+                 (g (add1 i) (cdr tests))])))))))
+
 
 (define input-filter
   (make-parameter (lambda (x) x)
@@ -79,6 +126,13 @@
                [(eof-object? c) (void)]
                [else (display c) (f)]))))))))
 
+(define (test-with-string-output test-id expr expected-output)
+   (run-compile expr)
+   (build)
+   (execute)
+   (unless (string=? expected-output (get-string))
+     (error 'test (format "output mismatch for test ~s, expected ~s, got ~s"
+        test-id expected-output (get-string)))))
 
 (define (emit . args)
   (apply fprintf (compile-port) args)
